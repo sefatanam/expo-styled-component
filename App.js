144b3be60462e076b5591e0557d31a2db2147c67
Sefat Anam <sefatanam@gmail.com>
2020-04-30 01:06:56 +0600

    Styled Component Practice => Complete

--- App.js
+++ App.js
@@ -4,6 +4,23 @@ import styled from "styled-components";
 import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
+  const recipies = [
+    {
+      name: "Burger",
+      info: "80 calories per 100gm",
+      image: require("./assets/Burger.jpg"),
+    },
+    {
+      name: "Burger",
+      info: "80 calories per 100gm",
+      image: require("./assets/Burger.jpg"),
+    },
+    {
+      name: "Burger",
+      info: "80 calories per 100gm",
+      image: require("./assets/Burger.jpg"),
+    },
+  ];
   return (
     <Container barStyle="light-content">
       <RecipeBackground source={require("./assets/Burger.jpg")}>
@@ -20,14 +37,85 @@ export default function App() {
               Burger
             </Text>
             <Divider />
-            <Text large bold>80 calories per 100gm</Text>
-            <Text small bold>3g Fat | 3g Protien | 8g Carbs</Text>
+            <Text large bold>
+              80 calories per 100gm
+            </Text>
+            <Text small bold>
+              3g Fat | 3g Protien | 8g Carbs
+            </Text>
           </MainRecipe>
+          <Button>
+            <Text large bold>
+              Learn More
+            </Text>
+          </Button>
         </SafeAreaView>
       </RecipeBackground>
+
+      <RecipeContainer>
+        <Text dark heavy large>
+          Recipes
+        </Text>
+        <Text dark large bold>
+          20 Recipes Available
+        </Text>
+
+        <Recipes>
+          {recipies.map((recipie, index) => {
+            return (
+              <Recipe key={index}>
+                <RecipeImage source={recipie.image} />
+                <RecipeInfo>
+                  <Text dark bold small>
+                    {recipie.name}
+                  </Text>
+                  <Text dark bold small>
+                    {recipie.info}
+                  </Text>
+                </RecipeInfo>
+                <AntDesign name="hearto" color="#000" />
+              </Recipe>
+            );
+          })}
+        </Recipes>
+      </RecipeContainer>
     </Container>
   );
 }
+
+const Recipe = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+const RecipeImage = styled.Image`
+  width: 68px;
+  height: 60px;
+  border-radius: 6px;
+`;
+const RecipeInfo = styled.View`
+flex:1;
+margin-left:12px
+`;
+
+const Recipes = styled.View`
+  margin-top: 16px;
+`;
+
+const RecipeContainer = styled.View`
+  margin-top: -24px;
+  padding: 32px;
+  border-top-left-radius: 32px;
+  border-top-right-radius: 24px;
+`;
+
+const Button = styled.TouchableOpacity`
+  margin: 0 0 48px 32px;
+  background-color: rgba(255, 255, 255, 0.5);
+  align-self: flex-start;
+  padding: 6px 18px;
+  border-radius: 100px;
+`;
 
 const Divider = styled.View`
   border-bottom-color: #fff;
@@ -62,7 +150,6 @@ const Text = styled.Text`
 
 const Container = styled.View`
   flex: 1;
-  backgroundColor: #fff;
 `;
 
 const RecipeBackground = styled.ImageBackground`
